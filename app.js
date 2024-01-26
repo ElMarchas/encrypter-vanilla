47889d78be8de34da25290ab24511888ada42ee4
ElMarchas <marchas53@hotmail.com>
2024-01-26 12:41:24 -0600

    put out  buttons from cards and implement foreach in lang array

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-import lang from "./lang.js";
+import lang from "./assets/lang.js";
+
+var data = {
+  languaje: "EN",
+  bntLang: document.getElementById("btnLang"),
+  bntPro: document.getElementById("btnPro"),
+  bntPro: document.getElementById("btnPro"),
+  btnEncry: document.getElementById("btnEncrypt"),
+  btnDecry: document.getElementById("btnDecrypt"),
+  btnSwap: document.getElementById("btnSwap"),
+  btnCopy: document.getElementById("btnCopy"),
+};
+
+const setLanguaje = (_lang) => {
+  if (_lang != "ES" && _lang != "EN") _lang = "ES";
+  let d = document;
+
+  Object.keys(lang[_lang]).forEach((key) => {
+    d.getElementById(key).innerText = lang[_lang][key];
+    if (key == "inputOut") {
+      d.getElementById(key).innerText = lang[_lang][key][0];
+    }
+  });
+};
+
+const changeLanguaje = () => {
+  if (data.languaje == "ES") data.languaje = "EN";
+  else data.languaje = "ES";
+  return setLanguaje(data.languaje);
+};
+
+const setup = () => {
+  setLanguaje(data.languaje);
+
+  data.bntLang.addEventListener("click", changeLanguaje);
+};
+
+setup();
+
+/*
 
 var maxNumber = 10;
 var targetNumber = 0;
@@ -32,3 +71,6 @@ const setup = () => {
 
 let test = document.getElementById("test");
 test.innerText = lang.ES.title;
+
+
+*/
---
+++ assets/lang.js
@@ -0,0 +1,28 @@
+const lang = {
+  EN: {
+    title: "Encrypter",
+    conditional: "🛈Lowercase and non-special only.",
+    btnLang: "Español",
+    btnPro: "Pro Version",
+    btnEncrypt: "Encrypt",
+    btnDecrypt: "Decrypt",
+    btnCopy: "Copy",
+    inputIn: "Enter Text",
+    inputOut: ["Encrypt", "Decrypt"],
+    footer: "by ElMarchas",
+  },
+  ES: {
+    title: "Cifrador",
+    conditional: " 🛈 Solo minusculas y sin acentos .",
+    btnLang: "English",
+    btnPro: "Versión Pro",
+    btnEncrypt: "Cifrar",
+    btnDecrypt: "Descifrar",
+    btnCopy: "Copiar",
+    inputIn: "Ingresa Texto",
+    inputOut: ["Cifrar", "Descifrar"],
+    footer: "Hecho por ElMarchas",
+  },
+};
+
+export default lang;
